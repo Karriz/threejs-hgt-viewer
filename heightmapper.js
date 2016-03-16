66c4a4b83d5069867fb67241be750eae8c3effc0
Karri Ojala <ojala.karri@gmail.com>
2016-03-16 20:58:06 +0200

    Multiple file loading

--- heightmapper.js
+++ heightmapper.js
@@ -4,29 +4,6 @@ function HeightMapper() {
 
     this.buildHeightMap = function (geometry,lat,lon,zoom) {
         var oReq = new XMLHttpRequest();
-        if (lat > 0) {
-            latstr = 'N'+(Math.abs(Math.floor(lat)));
-        }
-        else {
-            latstr = 'S'+(Math.abs(Math.floor(lat)));
-        }
-        if (lon > 0) {
-            if (lon < 180) {
-                lonstr = 'E0'+(Math.abs(Math.floor(lon)));
-            }
-            else {
-                lonstr = 'E'+(Math.abs(Math.floor(lon)));
-            }
-        }
-        else {
-            if (lon >= -179) {
-                lonstr = 'W0'+(Math.abs(Math.floor(lon)));
-            }
-            else {
-                lonstr = 'W'+Math.abs((Math.floor(lon)));
-            }
-        }
-        console.log('/Heightmaps/'+latstr+lonstr+'.hgt');
         
         size = Math.sqrt(geometry.vertices.length);
         
@@ -35,6 +12,14 @@ function HeightMapper() {
         
         w_lon = 360/(Math.pow(2,zoom-1));
         h_lat = Math.abs(Math.cos(lat*(Math.PI/180))*360/(Math.pow(2,zoom-1)));
+        
+        startlat = lat - (h_lat / 2);
+        endlat = lat + (h_lat / 2);
+        
+        console.log(startlat);
+        
+        startlon = lon - (w_lon / 2);
+        endlon = lon + (w_lon / 2);
         
         console.log(w_lon);
         console.log(h_lat);
@@ -48,85 +33,107 @@ function HeightMapper() {
         h_step = size/h_indices;
         
         console.log(h_step,w_step);
-     
-        center_r = 1201-Math.abs(1201*(lat - Math.floor(lat)));
-        center_c = Math.abs(1201*(lon - Math.floor(lon)));
         
-        console.log(center_r,center_c);
+        start_row = 1201 - Math.abs(1201*(endlat - Math.floor(endlat)));
+        start_column = Math.abs(1201*(startlon - Math.floor(startlon)));
         
-        start_r = center_r - (h_indices/2);
-        start_c = center_c - (w_indices/2);
+        console.log(start_row, start_column);
         
-        console.log(start_r,start_c);
-        
-        oReq.open("GET", '/Heightmaps/'+latstr+lonstr+'.hgt', true);
-        oReq.responseType = "arraybuffer";
-
-        oReq.onload = function (oEvent) {
-          var arrayBuffer = oReq.response;
-          if (arrayBuffer) {
-            var array = new Int16Array(arrayBuffer);
-            console.log(array.length);
-            console.log(geometry.vertices.length);
-            var datav=new DataView(arrayBuffer);
-            
-            h_i = 0;
-            w_i = 0;
-            
-            for (var i=0;i<size;i++) {
-                h_i = i/h_step;
-                
-                for (var j=0;j<size;j++) {
-                    w_i = j/w_step;
-                    
-                    actual_r = start_r + h_i;
-                    actual_c = start_c + w_i;
-                    
-                    c_left = actual_c - Math.floor(actual_c);
-                    r_up = actual_r - Math.floor(actual_r);
-                    
-                    actual_r = Math.floor(actual_r);
-                    actual_c = Math.floor(actual_c);
-                    
-                    actual_index_ul = Math.floor(actual_r*1201+actual_c);
-                    if (actual_c < 1200) {
-                        actual_index_ur = Math.floor(actual_r*1201+actual_c+1);
-                    }
-                    else {
-                        actual_index_ur = Math.floor(actual_r*1201+actual_c);
-                    }
-                    if (actual_r < 1200) {
-                        actual_index_dl = Math.floor((actual_r+1)*1201+actual_c);
-                    }
-                    else {
-                        actual_index_dl = Math.floor((actual_r)*1201+actual_c);
-                    }
-                    if (actual_r < 1200 && actual_c < 1200) {
-                        actual_index_dr = Math.floor((actual_r+1)*1201+actual_c+1);
-                    }
-                    else {
-                        actual_index_dr = Math.floor((actual_r)*1201+actual_c);
-                    }
-                    var h_ul = datav.getInt16(actual_index_ul*2,false);
-                    var h_ur = datav.getInt16(actual_index_ur*2,false);
-                    var h_dl = datav.getInt16(actual_index_dl*2,false);
-                    var h_dr = datav.getInt16(actual_index_dr*2,false);
-                    
-                    h = (1-c_left) * (1-r_up) * h_ul + (1-c_left) * r_up * h_dl + c_left * (1-r_up) * h_ur + c_left * r_up * h_dr;
-                    
-                    //console.log(h);
-                    
-                    geometry.vertices[i*size+j].setZ(h*scale);
+        for (curlat = Math.floor(startlat); curlat <= Math.floor(endlat); curlat++) {
+            for (curlon = Math.floor(startlon); curlon <= Math.floor(endlon); curlon++) {
+                if (curlat > 0) {
+                    latstr = 'N'+(Math.abs(Math.floor(curlat)));
                 }
-            }
-            geometry.verticesNeedUpdate = true;
-            geometry.computeFaceNormals();
-            geometry.computeVertexNormals();
-          }
-        };
+                else {
+                    latstr = 'S'+(Math.abs(Math.floor(curlat)));
+                }
+                if (curlon > 0) {
+                    if (curlon < 180) {
+                        lonstr = 'E0'+(Math.abs(Math.floor(curlon)));
+                    }
+                    else {
+                        lonstr = 'E'+(Math.abs(Math.floor(curlon)));
+                    }
+                }
+                else {
+                    if (curlon >= -179) {
+                        lonstr = 'W0'+(Math.abs(Math.floor(curlon)));
+                    }
+                    else {
+                        lonstr = 'W'+Math.abs((Math.floor(curlon)));
+                    }
+                }
+                console.log('/Heightmaps/'+latstr+lonstr+'.hgt');
+                
+                oReq.open("GET", '/Heightmaps/'+latstr+lonstr+'.hgt', true);
+                oReq.responseType = "arraybuffer";
+                
+                oReq.onload = function (oEvent) {
+                  var arrayBuffer = oReq.response;
+                  if (arrayBuffer) {
+                    var array = new Int16Array(arrayBuffer);
+                    console.log(array.length);
+                    console.log(geometry.vertices.length);
+                    var datav=new DataView(arrayBuffer);
+                    
+                    h_i = 0;
+                    w_i = 0;
+                    
+                    for (var i=0;i<size;i++) {
+                        h_i = i/h_step;
+                        
+                        for (var j=0;j<size;j++) {
+                            w_i = j/w_step;
+                            
+                            actual_r = start_row + h_i;
+                            actual_c = start_column + w_i;
+                            
+                            c_left = actual_c - Math.floor(actual_c);
+                            r_up = actual_r - Math.floor(actual_r);
+                            
+                            actual_r = Math.floor(actual_r);
+                            actual_c = Math.floor(actual_c);
+                            
+                            actual_index_ul = Math.floor(actual_r*1201+actual_c);
+                            if (actual_c < 1200) {
+                                actual_index_ur = Math.floor(actual_r*1201+actual_c+1);
+                            }
+                            else {
+                                actual_index_ur = Math.floor(actual_r*1201+actual_c);
+                            }
+                            if (actual_r < 1200) {
+                                actual_index_dl = Math.floor((actual_r+1)*1201+actual_c);
+                            }
+                            else {
+                                actual_index_dl = Math.floor((actual_r)*1201+actual_c);
+                            }
+                            if (actual_r < 1200 && actual_c < 1200) {
+                                actual_index_dr = Math.floor((actual_r+1)*1201+actual_c+1);
+                            }
+                            else {
+                                actual_index_dr = Math.floor((actual_r)*1201+actual_c);
+                            }
+                            var h_ul = datav.getInt16(actual_index_ul*2,false);
+                            var h_ur = datav.getInt16(actual_index_ur*2,false);
+                            var h_dl = datav.getInt16(actual_index_dl*2,false);
+                            var h_dr = datav.getInt16(actual_index_dr*2,false);
+                            
+                            h = (1-c_left) * (1-r_up) * h_ul + (1-c_left) * r_up * h_dl + c_left * (1-r_up) * h_ur + c_left * r_up * h_dr;
+                            
+                            //console.log(h);
+                            
+                            geometry.vertices[i*size+j].setZ(h*scale);
+                        }
+                    }
+                    geometry.verticesNeedUpdate = true;
+                    geometry.computeFaceNormals();
+                    geometry.computeVertexNormals();
+                  }
+                };
 
-        oReq.send(null);
-         
+                oReq.send(null);
+            }    
+        } 
     };
 
 
